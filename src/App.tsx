414937609f2d901fb3529ef9ec7842252358674b
giangann <70595717+giangann@users.noreply.github.com>
2023-03-02 13:07:31 +0700

    Initial carousel

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import { useEffect } from 'react';
 
 import { Home } from './screens';
+import { useTheme, useMediaQuery } from '@mui/material';
 
 function App() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'), { noSsr: true });
   useEffect(() => {
     const script = document.createElement('script');
 
-    script.src = '/index.js';
+    script.src = isMobile ? '/carouselMobile.js' : '/index.js';
     script.async = true;
 
     document.body.appendChild(script);
--- src/components/Carousel3dImage.tsx
+++ src/components/Carousel3dImage.tsx
@@ -3,8 +3,12 @@ import './carousel3d/carousel3d.css';
 import { useEffect } from 'react';
 
 import { PREVIEW_BG_FOLDER_PATH } from '../constants';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 export const Carousel3dImage = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'), { noSsr: true });
+
   const bgImageArr: string[] = [];
   for (let i = 1; i < 18; i++) {
     bgImageArr.push(`${PREVIEW_BG_FOLDER_PATH}/background${i > 9 ? i - 9 : i}.jpg`);
@@ -13,7 +17,7 @@ export const Carousel3dImage = () => {
   useEffect(() => {
     const script = document.createElement('script');
 
-    script.src = '/index.js';
+    script.src = isMobile ? 'carouselMobile.js' : '/index.js';
     script.async = true;
 
     document.body.appendChild(script);
@@ -23,24 +27,44 @@ export const Carousel3dImage = () => {
     };
   }, []);
   return (
-    <div style={{ overflowX: 'hidden' }}>
-      <div id="drag-container">
-        <div id="spin-container">
-          {bgImageArr.map((image, index) => (
-            <img
-              className="carousel-3d-image"
-              src={image}
-              key={index}
-              alt=""
-              data-index={index}
-            />
-          ))}
+    <>
+      {isMobile ? (
+        <>
+          <div id="coverflow">
+            <section data-cover="http://a2.mzstatic.com/us/r1000/060/Features/1c/df/c3/dj.sahccbiy.170x170-75.jpg"></section>
+            <section data-cover="http://a3.mzstatic.com/us/r1000/102/Music/95/45/3f/mzi.xqnmexwi.170x170-75.jpg"></section>
+            <section data-cover="http://a4.mzstatic.com/us/r1000/014/Music/ea/6f/96/mzi.egqrvlca.170x170-75.jpg"></section>
+            <section data-cover="http://a3.mzstatic.com/us/r1000/096/Music/1a/86/1f/mzi.dcotnnwo.170x170-75.jpg"></section>
+            <section data-cover="http://a4.mzstatic.com/us/r1000/094/Music/a8/8b/db/mzi.dyzjtwow.170x170-75.jpg"></section>
+            <section data-cover="http://a1.mzstatic.com/us/r1000/024/Music/81/54/3b/mzi.cceuzwlz.170x170-75.jpg"></section>
+            <section data-cover="http://a5.mzstatic.com/us/r1000/103/Music/2f/08/9e/mzi.gxjxokia.170x170-75.jpg"></section>
+          </div>
 
-          {/* <!-- Text at center of ground --> */}
-          <p>3D Tiktok Carousel</p>
+          <nav id="controls">
+            <a id="prev">Left</a> & <a id="next">Right</a>
+          </nav>
+        </>
+      ) : (
+        <div style={{ overflowX: 'hidden' }}>
+          <div id="drag-container">
+            <div id="spin-container">
+              {bgImageArr.map((image, index) => (
+                <img
+                  className="carousel-3d-image"
+                  src={image}
+                  key={index}
+                  alt=""
+                  data-index={index}
+                />
+              ))}
+
+              {/* <!-- Text at center of ground --> */}
+              <p>3D Tiktok Carousel</p>
+            </div>
+            <div id="ground"></div>
+          </div>
         </div>
-        <div id="ground"></div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
